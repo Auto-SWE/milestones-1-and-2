0cae3b31b5738e9bb6c11eb714c669ce5b081680
JL <115737916+yikesboy@users.noreply.github.com>
2026-04-21 10:50:11 +0200

    build: flake now supports darwin macs (atleast it should, lol)

--- flake.nix
+++ flake.nix
@@ -7,36 +7,46 @@
 
   outputs = { nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      runtimeLibs = pkgs.lib.makeLibraryPath [
-        pkgs.stdenv.cc.cc.lib
-        pkgs.zlib
-        pkgs.openssl
-        pkgs.libffi
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
       ];
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.python313
-          pkgs.uv
-          pkgs.cacert
-          pkgs.gcc
-          pkgs.git
-          pkgs.pkg-config
-          pkgs.openssl
-          pkgs.zlib
-          pkgs.libffi
-          pkgs.stdenv.cc.cc.lib
-        ];
 
-        shellHook = ''
-          export LD_LIBRARY_PATH="${runtimeLibs}:''${LD_LIBRARY_PATH:-}"
-          export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-          export UV_LINK_MODE=copy
-          export UV_PROJECT_ENVIRONMENT=.venv
-          export PYTHONNOUSERSITE=1
-        '';
-      };
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          runtimeLibs = pkgs.lib.makeLibraryPath [
+            pkgs.stdenv.cc.cc.lib
+            pkgs.zlib
+            pkgs.openssl
+            pkgs.libffi
+          ];
+        in {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.python313
+              pkgs.uv
+              pkgs.cacert
+              pkgs.git
+              pkgs.pkg-config
+              pkgs.openssl
+              pkgs.zlib
+              pkgs.libffi
+              pkgs.stdenv.cc
+            ];
+
+            shellHook = ''
+              ${pkgs.lib.optionalString pkgs.stdenv.isLinux ''
+                export LD_LIBRARY_PATH="${runtimeLibs}:''${LD_LIBRARY_PATH:-}"
+              ''}
+              export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+              export UV_LINK_MODE=copy
+              export UV_PROJECT_ENVIRONMENT=.venv
+              export PYTHONNOUSERSITE=1
+            '';
+          };
+        });
     };
 }
