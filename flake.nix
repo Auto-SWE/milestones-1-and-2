25257f75497924b8d652506d7e487caf407ab23d
yikesboy <115737916+yikesboy@users.noreply.github.com>
2026-05-01 18:16:55 +0000

    build: updated flake.nix

--- flake.nix
+++ flake.nix
@@ -7,26 +7,15 @@
 
   outputs = { nixpkgs, ... }:
     let
-      systems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
-
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
       devShells = forAllSystems (system:
         let
           pkgs = import nixpkgs { inherit system; };
-          runtimeLibs = pkgs.lib.makeLibraryPath [
-            pkgs.stdenv.cc.cc.lib
-            pkgs.zlib
-            pkgs.openssl
-            pkgs.libffi
-          ];
         in {
           default = pkgs.mkShell {
             packages = [
-              pkgs.python313
               pkgs.uv
               pkgs.cacert
               pkgs.git
@@ -34,13 +23,11 @@
               pkgs.openssl
               pkgs.zlib
               pkgs.libffi
-              pkgs.stdenv.cc
             ];
 
             shellHook = ''
-              ${pkgs.lib.optionalString pkgs.stdenv.isLinux ''
-                export LD_LIBRARY_PATH="${runtimeLibs}:''${LD_LIBRARY_PATH:-}"
-              ''}
+              unset LD_LIBRARY_PATH
+
               export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
               export UV_LINK_MODE=copy
               export UV_PROJECT_ENVIRONMENT=.venv
